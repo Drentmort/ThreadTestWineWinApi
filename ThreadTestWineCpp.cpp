c84fbb9146e2a1c7c6e4469fcb93e1cc1c46530e
Anton Andryakov <60717352+Drentmort@users.noreply.github.com>
2023-05-18 14:17:33 +0300

    Add files via upload

---
+++ ThreadTestWineCpp.cpp
@@ -0,0 +1,55 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <Windows.h>
+#include <process.h>
+#include <stdio.h>
+#include <conio.h>
+
+void StartTest(int threadCount);
+void BeginNewThread();
+void ExecuteEnterReleaseAction();
+
+DWORD WINAPI TargetTestFunc(LPVOID t);
+
+int main(int argc, char* argv[])
+{
+	printf("Enter thread count\n");
+	unsigned int threadCount = 100;
+	scanf("%i", &threadCount);
+	StartTest(threadCount);
+	_getch();
+	return 0;
+}
+
+void StartTest(int threadCount) 
+{
+	for (int i = 0; i < threadCount; i++) 
+	{
+		BeginNewThread();
+	}
+}
+
+void BeginNewThread()
+{
+	HANDLE thread;
+	thread = CreateThread(NULL, 0, TargetTestFunc, NULL, 0, NULL);
+	printf("Thread created %d \n", GetCurrentThreadId());
+}
+
+void ExecuteEnterReleaseAction() 
+{
+	HANDLE mutex = CreateMutex(NULL,FALSE,NULL);
+	if (mutex != NULL) 
+	{
+		WaitForSingleObject(mutex, INFINITE);
+		ReleaseMutex(mutex);
+		CloseHandle(mutex);
+	}
+}
+
+DWORD WINAPI TargetTestFunc(LPVOID t)
+{
+	ExecuteEnterReleaseAction();
+	BeginNewThread();
+	ExitThread(0);
+}
